5540b3be1882da2d20f484598c4d93524ad714f2
saroshkhandev <saroshabdullah2013@gmail.com>
2022-03-28 15:47:01 +0530

    Added Users + Products Table

--- src/page/Products/Products.js
+++ src/page/Products/Products.js
@@ -1,10 +1,53 @@
-import { VStack } from '@chakra-ui/react'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  VStack,
+} from '@chakra-ui/react'
 import React from 'react'
 
 const Products = () => {
   return (
-    <VStack h="full" w="full">
-      <div>Products</div>
+    <VStack h="full" w="full" p={10}>
+      <Table variant="striped" colorScheme="teal">
+        <TableCaption>Products Stock Availability</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Product Name</Th>
+            <Th>Section</Th>
+            <Th isNumeric>Amount</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>Mi Tv</Td>
+            <Td>Electronics</Td>
+            <Td isNumeric>24</Td>
+          </Tr>
+          <Tr>
+            <Td>Denver</Td>
+            <Td>Beauty Products</Td>
+            <Td isNumeric>253</Td>
+          </Tr>
+          <Tr>
+            <Td>Keyring</Td>
+            <Td>Accessories</Td>
+            <Td isNumeric>4</Td>
+          </Tr>
+        </Tbody>
+        <Tfoot>
+          <Tr>
+            <Th>Product Name</Th>
+            <Th>Section</Th>
+            <Th isNumeric>Amount</Th>
+          </Tr>
+        </Tfoot>
+      </Table>
     </VStack>
   )
 }
--- src/page/Users/Users.js
+++ src/page/Users/Users.js
@@ -1,12 +1,65 @@
-import { VStack } from '@chakra-ui/react'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  VStack,
+} from '@chakra-ui/react'
 import React from 'react'
 
 const Users = () => {
-    return (
-      <VStack h="full" w="full">
-        <div>Users</div>
-      </VStack>
-    )
+  return (
+    <VStack h="full" w="full" p={10}>
+      <Table variant="striped" colorScheme="teal">
+        <TableCaption>Users List</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>User Name</Th>
+            <Th>Pending Order</Th>
+            <Th isNumeric>Joined</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>Sarosh</Td>
+            <Td>Yes</Td>
+            <Td isNumeric>24-Apr</Td>
+          </Tr>
+          <Tr>
+            <Td>Vishal Singh</Td>
+            <Td>No</Td>
+            <Td isNumeric>28-Apr</Td>
+          </Tr>
+          <Tr>
+            <Td>Sneha</Td>
+            <Td>No</Td>
+            <Td isNumeric>21-Apr</Td>
+          </Tr>
+          <Tr>
+            <Td>Aariz</Td>
+            <Td>Yes</Td>
+            <Td isNumeric>22-Apr</Td>
+          </Tr>
+          <Tr>
+            <Td>Priyanshu</Td>
+            <Td>Yes</Td>
+            <Td isNumeric>23-Apr</Td>
+          </Tr>
+        </Tbody>
+        <Tfoot>
+          <Tr>
+            <Th>User Name</Th>
+            <Th>Pending Order</Th>
+            <Th isNumeric>Joined</Th>
+          </Tr>
+        </Tfoot>
+      </Table>
+    </VStack>
+  )
 }
 
 export default Users
