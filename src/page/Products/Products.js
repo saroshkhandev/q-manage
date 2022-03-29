9c0e3bced55962a4968a83889d874be63cab20e5
saroshkhandev <saroshabdullah2013@gmail.com>
2022-03-29 10:36:56 +0530

    Minor tweaks in dash + pages

--- src/page/Products/Products.js
+++ src/page/Products/Products.js
@@ -8,46 +8,62 @@ import {
   Td,
   TableCaption,
   VStack,
+  Button,
+  Box,
 } from '@chakra-ui/react'
 import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
 
 const Products = () => {
   return (
-    <VStack h="full" w="full" p={10}>
-      <Table variant="striped" colorScheme="teal">
-        <TableCaption>Products Stock Availability</TableCaption>
-        <Thead>
-          <Tr>
-            <Th>Product Name</Th>
-            <Th>Section</Th>
-            <Th isNumeric>Amount</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td>Mi Tv</Td>
-            <Td>Electronics</Td>
-            <Td isNumeric>24</Td>
-          </Tr>
-          <Tr>
-            <Td>Denver</Td>
-            <Td>Beauty Products</Td>
-            <Td isNumeric>253</Td>
-          </Tr>
-          <Tr>
-            <Td>Keyring</Td>
-            <Td>Accessories</Td>
-            <Td isNumeric>4</Td>
-          </Tr>
-        </Tbody>
-        <Tfoot>
-          <Tr>
-            <Th>Product Name</Th>
-            <Th>Section</Th>
-            <Th isNumeric>Amount</Th>
-          </Tr>
-        </Tfoot>
-      </Table>
+    <VStack h="full" w="full">
+      <Box p="20px" m={2} bg="DFDFDE" w="full">
+        <Button
+          align="left"
+          bg="#4D96FF"
+          color="#FFFFFF"
+          as={RouterLink}
+          to="/products/add"
+        >
+          Add Product
+        </Button>
+      </Box>
+      <Box p={4} w="full">
+        <Table variant="striped" colorScheme="teal">
+          <TableCaption>Products Stock Availability</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Product Name</Th>
+              <Th>Section</Th>
+              <Th isNumeric>Amount</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>Mi Tv</Td>
+              <Td>Electronics</Td>
+              <Td isNumeric>24</Td>
+            </Tr>
+            <Tr>
+              <Td>Denver</Td>
+              <Td>Beauty Products</Td>
+              <Td isNumeric>253</Td>
+            </Tr>
+            <Tr>
+              <Td>Keyring</Td>
+              <Td>Accessories</Td>
+              <Td isNumeric>4</Td>
+            </Tr>
+          </Tbody>
+          <Tfoot>
+            <Tr>
+              <Th>Product Name</Th>
+              <Th>Section</Th>
+              <Th isNumeric>Amount</Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </Box>
     </VStack>
   )
 }
---
+++ src/page/Products/components/AddProduct.js
@@ -0,0 +1,73 @@
+import { VStack } from '@chakra-ui/react'
+import { Formik } from 'formik'
+import React from 'react'
+import * as Yup from 'yup'
+
+
+const AddProduct = () => {
+  return (
+    <VStack>
+      <Formik
+        initialValues={{ email: '' }}
+        onSubmit={async values => {
+          await new Promise(resolve => setTimeout(resolve, 500))
+          alert(JSON.stringify(values, null, 2))
+        }}
+        validationSchema={Yup.object().shape({
+          email: Yup.string().email().required('Required'),
+        })}
+      >
+        {props => {
+          const {
+            values,
+            touched,
+            errors,
+            dirty,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            handleReset,
+          } = props
+          return (
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="email" style={{ display: 'block' }}>
+                Email
+              </label>
+              <input
+                id="email"
+                placeholder="Enter your email"
+                type="text"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={
+                  errors.email && touched.email
+                    ? 'text-input error'
+                    : 'text-input'
+                }
+              />
+              {errors.email && touched.email && (
+                <div className="input-feedback">{errors.email}</div>
+              )}
+
+              <button
+                type="button"
+                className="outline"
+                onClick={handleReset}
+                disabled={!dirty || isSubmitting}
+              >
+                Reset
+              </button>
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </form>
+          )
+        }}
+      </Formik>
+    </VStack>
+  )
+}
+
+export default AddProduct
