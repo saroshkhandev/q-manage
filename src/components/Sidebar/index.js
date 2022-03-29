f7fef468d15f374a761769356f9607a12fa68ce8
saroshkhandev <saroshabdullah2013@gmail.com>
2022-03-29 11:32:36 +0530

    New Fix

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <Box w="200px" minW="200px" bg="#edf2f7" p={8} h="100vh">
+    <Box w="200px" minW="200px" bg="#B8FFF9" p={8} h="100vh">
       <VStack gap={3} align="left">
         <Text fontWeight="bold" fontSize="lg">
           Queue Manager
@@ -14,7 +14,10 @@ const Sidebar = () => {
         >
           Dashboard
         </Link>
-        <Link href="/sales" _hover={{ textDecoration: 'none', fontWeight: 'bold' }}>
+        <Link
+          href="/sales"
+          _hover={{ textDecoration: 'none', fontWeight: 'bold' }}
+        >
           Sales
         </Link>
         <Link
--- src/page/Dashboard/Dashboard.js
+++ src/page/Dashboard/Dashboard.js
@@ -4,51 +4,57 @@ import { Link as RouterLink } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
-    <VStack h="full" p={10}>
-      <HStack gap={5}>
+    <VStack h="full" p={10} bg="#EFFFFD" w="full">
+      <HStack gap={5} justify="space-between">
         <Box
           bg="#2D8EFF"
           p={5}
           w="300px"
-          h="100px"
+          h="200px"
           color="white"
-          borderRadius={5}
+          borderRadius="20px"
           _hover={{ border: '1px solid black' }}
         >
-          <Text fontSize="xl" fontWeight="bold">
-            Active Users
-          </Text>
-          <Text>Active: 4</Text>
+          <VStack align="center" margin="auto 0">
+            <Text fontSize="xl" fontWeight="bold">
+              Active Users
+            </Text>
+            <Text>Active: 4</Text>
+          </VStack>
         </Box>
         <Box
           bg="#2D8EFF"
           p={5}
           w="300px"
-          h="100px"
+          h="200px"
           color="white"
-          borderRadius={5}
+          borderRadius="20px"
           as={RouterLink}
           to="/users"
           _hover={{ border: '1px solid black' }}
         >
-          <Text fontSize="xl" fontWeight="bold">
-            Customers
-          </Text>
+          <VStack align="center" margin="auto 0">
+            <Text fontSize="xl" fontWeight="bold">
+              Customers
+            </Text>
+          </VStack>
         </Box>
         <Box
           bg="#2D8EFF"
           p={5}
           w="300px"
-          h="100px"
+          h="200px"
           color="white"
-          borderRadius={5}
+          borderRadius="20px"
           as={RouterLink}
           to="/sales"
           _hover={{ border: '1px solid black' }}
         >
-          <Text fontSize="xl" fontWeight="bold">
-            Sales
-          </Text>
+          <VStack align="center" margin="auto 0">
+            <Text fontSize="xl" fontWeight="bold">
+              Sales
+            </Text>
+          </VStack>
         </Box>
       </HStack>
     </VStack>
